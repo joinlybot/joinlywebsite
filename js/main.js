d21c2f1f3d88893c097db9189bd2dfae8a082e55
Joinly <lelerrecarole@gmail.com>
2026-08-20 21:24:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -310,3 +310,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
+fetch("/components/navbar.html")
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById("navbar").innerHTML = html;
+    });
